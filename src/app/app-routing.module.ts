76739a10b6017def59c258319124a301545e43df
percivalbragg <percivalbragg@yahoo.com>
2021-04-04 22:03:23 -0500

    Portfolio coding completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
+import { AboutComponent } from './about/about.component';
+import { StrengthsComponent } from './strengths/strengths.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
